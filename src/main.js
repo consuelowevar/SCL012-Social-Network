49531348e894b5169e755b1b7631f2d89111e219
bdiniscia <bdiniscia@gmail.com>
2020-01-16 10:01:20 -0300

    Instalación de Firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 // Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const sendButton = document.getElementById('send');
+
+
+sendButton.addEventListener('click', () => {
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // [END_EXCLUDE]
+  });
+
+});
