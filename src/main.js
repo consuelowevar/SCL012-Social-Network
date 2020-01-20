b37c0009e5f1d10bb278b0224335d822d5799454
bdiniscia <bdiniscia@gmail.com>
2020-01-20 09:04:28 -0300

    Generado dinámico del log in y sign up

--- src/main.js
+++ src/main.js
@@ -3,17 +3,61 @@ import { myFunction } from './lib/index.js';
 
 myFunction();
 
+const authSection = document.getElementById('authSection'); // Sección de registro
+
+// Función que carga el Sign In
+function loadSignIn() {
+  const sbSingIn = document.createElement('button');
+  sbSingIn.innerText = 'Entrar';
+  sbSingIn.addEventListener('click', () => {
+    sendButtonLogIn();
+  });
+  const toggleToSignUp = document.createElement('button');
+  toggleToSignUp.innerHTML = 'No tengo cuenta';
+  toggleToSignUp.addEventListener('click', () => {
+    loadSignUp();
+  });
+  authSection.innerHTML = `
+    <h1>Log in</h1>
+    <input type="email" id="emailLogIn" placeholder="Email">
+    <input type="password" id="passwordLogIn" placeholder="Contraseña">
+  `;
+  authSection.appendChild(sbSingIn);
+  authSection.appendChild(toggleToSignUp);
+}
+
+// Función que carga el Sign Up
+function loadSignUp() {
+  const sb = document.createElement('button');
+  sb.innerText = 'Registrarme';
+  sb.addEventListener('click', () => {
+    sendButton();
+  });
+  const toggleToSignIn = document.createElement('button');
+  toggleToSignIn.innerHTML = 'Ya tengo cuenta';
+  toggleToSignIn.addEventListener('click', () => {
+    loadSignIn();
+  });
+
+  authSection.innerHTML = `
+      <h1>Sign up</h1>
+      <input type="email" id="email" placeholder="Email">
+      <input type="password" id="password" placeholder="Contraseña">
+  `;
+  authSection.appendChild(sb);
+  authSection.appendChild(toggleToSignIn);
+}
 
 // Crear y Registrar usuario con Firebase
-const sendButton = document.getElementById('send');
-
-sendButton.addEventListener('click', () => {
+const sendButton = () => {
+  debugger;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(() => {
+    .then((user) => {
       emailVerification();
+      afterLogIn(user);
     })
     .catch((error) => {
     // Handle Errors here.
@@ -23,12 +67,11 @@ sendButton.addEventListener('click', () => {
       console.log(errorMessage);
     // ...
     });
-});
+};
+
 
 // Loggear usuario con Firebase
-const sendButtonLogIn = document.getElementById('sendLogIn');
-
-sendButtonLogIn.addEventListener('click', () => {
+const sendButtonLogIn = () => {
   const email = document.getElementById('emailLogIn').value;
   const password = document.getElementById('passwordLogIn').value;
 
@@ -41,7 +84,7 @@ sendButtonLogIn.addEventListener('click', () => {
       console.log(errorMessage);
     // ...
     });
-});
+};
 
 // Observador que te dice si hay usuario logueado o no
 function observerAuth() {
@@ -60,6 +103,7 @@ function observerAuth() {
       const providerData = user.providerData;
       // ...
     } else {
+      loadSignUp();
       // User is signed out.
       console.log('No existe usuario activo');
       // ...
@@ -78,7 +122,7 @@ function afterLogIn(user) {
     <button id='closeSession'>Cerrar Sesión</button>`;
 
     const closeSession = document.querySelector('#closeSession');
-    closeSession.addEventListener('click', (er) => {
+    closeSession.addEventListener('click', () => {
       firebase.auth().signOut()
         .then(() => {
           console.log('Saliendo');
@@ -86,10 +130,10 @@ function afterLogIn(user) {
         .catch((error) => {
           console.log(error);
         });
-      er.preventDefault();
     });
   } else {
     console.log('No está verificado');
+    contentPage.innerHTML = '<p>Verifica tu mail para poder entrar a la aplicación</p>';
   }
 }
 
